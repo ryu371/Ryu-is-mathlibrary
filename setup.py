dcaa1ff6637918d80d90ec5ed52004764dd13bd6
ryu371 <longzhijiedaojian@gmail.com>
2026-03-13 21:56:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="RyuEngine",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+)
